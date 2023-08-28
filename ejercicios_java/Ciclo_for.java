84b195615b96ff2ace2ece3656c9b19f54ec074c
Santiago Munoz <Santiagoml222@gmail.com>
2023-08-28 17:35:55 -0500

    probando el ciclo en java

---
+++ ejercicios_java/Ciclo_for.java
@@ -0,0 +1,15 @@
+package ejercicios_java;
+
+/**
+ * Ciclo_for
+ */
+
+public class Ciclo_for {
+
+    public static void main(String[] args){
+        for(int i =1; i < 4; i ++){
+            System.out.print(i +",");
+        }
+    }
+    
+}
